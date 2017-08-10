b1e50d7b81d4c8080e77b625511e323e1e7734a3
oustrin <30787967+oustrin@users.noreply.github.com>
2017-08-09 21:24:36 -0700

    Add files via upload

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerController : MonoBehaviour {
+	public float movespeed = 10.0f;
+	public Rigidbody2D playerRB;
 
+	void Start () {
+
+//      Setting up Movement Animations
+//		Animator playeranim;
+		playerRB = GetComponent<Rigidbody2D>();
+		playerRB.constraints = RigidbodyConstraints2D.FreezeRotation;
+	}
 	// Update is called once per frame
 	void Update () {
-		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 10.0f;
-		var y = Input.GetAxis ("Vertical") * Time.deltaTime * 10.0f;
-	
-		transform.Translate (x, 0, 0);
-		transform.Translate (0, y, 0);
+//		transform.Translate (x, 0, 0);
+//		transform.Translate (0, y, 0);
+
+		Vector2 movement = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+
+		playerRB.velocity = movement * movespeed;
 	}
 }
